983465fc702d80bc2801916d15da54da21759862
beneti <beneti@beneti.com.br>
2012-12-04 21:42:10 -0200

    Improving the code

--- insertion_sort/insertion_sort.rb
+++ insertion_sort/insertion_sort.rb
@@ -3,10 +3,8 @@ class Array
     for i in 1..self.length-1
       j = i
       val = self[i]
-      while j > 0 && self[j-1] > val do
-        bigger = self[j-1]
-        self[j-1] = self[j]
-        self[j] = bigger
+      while j > 0 && (self[j-1] <=> val) == 1  do
+        self[j-1], self[j] = self[j], self[j-1]
         j -= 1
       end
       self[j] = val
