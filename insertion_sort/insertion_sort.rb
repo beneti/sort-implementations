b6a0cc43cc84d5c3ef5f39dadd67dc2f21deda60
beneti <beneti@beneti.com.br>
2012-12-03 23:52:49 -0200

    Insertion Sort

---
+++ insertion_sort/insertion_sort.rb
@@ -0,0 +1,16 @@
+class Array
+  def insertion_sort
+    for i in 1..self.length-1
+      j = i
+      val = self[i]
+      while j > 0 && self[j-1] > val do
+        bigger = self[j-1]
+        self[j-1] = self[j]
+        self[j] = bigger
+        j -= 1
+      end
+      self[j] = val
+    end
+    self
+  end
+end
